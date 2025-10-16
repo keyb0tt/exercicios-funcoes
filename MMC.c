c45b36e0a31ff2fbe5955b721a6cba78301f2157
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-16 08:36:51 -0300

    .

--- MMC.c
+++ MMC.c
@@ -3,44 +3,54 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int mmcCalc(int n1, int n2){
-    int mmc = 0, div = 2, arraySize = 0;
-    int fatoresPrimos[arraySize];
+int mdcCalc(int n1, int n2){
+    int menorValor = n1;
 
-    while(true){
-        if(n1 % div == 0){
-            n1 /= div;
-            fatoresPrimos[arraySize] = div;
-        }
-        if(n2 % div == 0){
-            n2 /= div;
-            fatoresPrimos[arraySize] = div;
-        }
-
-        arraySize++;
-        div++;
+    if(n2 < n1){
+        menorValor = n2;
     }
 
-    for(int i = 0; i <= 2; i++){
-        printf(" %d ", fatoresPrimos[i]);
+    for(int i = menorValor; i >= 0; i--){
+        if(n1 % i == 0 && n2 % i == 0){
+            return i;
+        }
     }
 
     return 0;
 }
 
+int mmcCalc(int n1, int n2){
+    int mdc = mdcCalc(n1, n2), mmc = (n1 * n2) / mdc;
+    
+    return mmc;
+}
+
 int main(){
     int n1, n2;
 
-    system("clear");
-    printf("\nMínimo Múltiplo Comum (MMC)\n\n");
     
-    printf("Insira o 1° número: ");
-    scanf("%d", &n1);
-    printf("\n");
-    printf("Insira o 2° número: ");
-    scanf("%d", &n2);
+    for(int i = 1; i <= 2; i++){
+        system("clear");
+        printf("\n--------------------------------------------------\n");
+        printf("\nMínimo Múltiplo Comum (MMC)\n");
+        if(i == 1){
+            printf("\nInsira o %d° número: ", i);
+            scanf("%d", &n1);
+            system("clear");
+            continue;
+        } else {
+            printf("\nInsira o %d° número: ", i);
+            scanf("%d", &n2);
+            system("clear");
 
-    printf("\nMMC = %d\n\n", mmcCalc(n1, n2));
+        }
 
+        printf("\n--------------------------------------------------\n");
+        printf("\nMínimo Múltiplo Comum (MMC)\n");
+        printf("\nO MMC de %d e %d é igual a %d\n", n1, n2, mmcCalc(n1, n2));
+        printf("\n--------------------------------------------------\n\n");
+    }
+    
+    
     return 0;
 }
